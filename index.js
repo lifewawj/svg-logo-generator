a7b173a66ae56d5faba9aecba1430705b75950f4
lifewawj <arevaloaj01@gmail.com>
2023-12-19 03:31:54 -0800

    NPM Packages needed ADDED to index.js, along with Comments of functions to create

---
+++ index.js
@@ -0,0 +1,26 @@
+// TODO: NPM Packages needed for project
+const fs = require('fs');
+const inquirer = require('inquirer');
+
+
+
+// TODO: A function that collects the user Input, storing the questions an array of objects of each question
+// text
+
+// text-color
+
+// shape-color
+
+// shape
+
+
+// A function that validates the User Input's
+// If user input > 0 && user input < 4
+
+
+
+// TODO: A function to write the .svg file with the data
+
+
+// TODO: A function to init the app
+
